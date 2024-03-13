cab5b44550013922b259f10cd8eba20badca76dc
Alice <alice.al.moradiya@gmail.com>
2024-03-13 03:40:41 -0600

    editing Home page content

--- my-app/src/app/home.js
+++ my-app/src/app/home.js
@@ -7,7 +7,11 @@ const HomePageContent = () => {
       <div id="home-content" className="home-content text-white"> 
         <h1>Alice Moradiya</h1>
         <h2>Web Developer | Software Developer</h2>
-        <p>I am a student at the Concordia university, pursuing a bachelor degree in Information technology.</p>
+        <p>Results-oriented IT professional, demonstrated success as a Software Developer at Techeshta</p>
+        <p>contributing to a 25% increase in user adoption. Passionate about applying technical skills in software development,</p>
+        <p>database management, and networking. Possesses excellent interpersonal skills, strong communication, and a collaborative approach,</p>
+        <p>fostering positive working relationships within cross-functional teams. Demonstrated ability to adapt quickly, </p>
+        <p>solve complex problems, and show attention to detail in project execution.</p>
         <button>RESUME</button>
       </div>
 
