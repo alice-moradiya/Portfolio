21c2a8adce96d0255b107e6719cdeed6528793eb
Alice <alice.al.moradiya@gmail.com>
2024-03-14 03:43:33 -0600

    Editing  button ui

--- my-app/src/app/home.js
+++ my-app/src/app/home.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { WavyBackground } from '@/app/utils/components/ui/wavy-background';
 import '../app/globals.css';
+import { buttons } from '@/app/utils/components/ui/tailwindcss-buttons.tsx';
 
 const HomePageContent = () => {
+  const borderMagicButton = buttons.find(button => button.name === 'Border Magic').component;
   return (
       <div id="home-content" className="home-content text-white"> 
         <h1>Alice Moradiya</h1>
@@ -10,7 +12,7 @@ const HomePageContent = () => {
         <p>Dynamic Software Developer with a track record of driving user adoption and enhancing </p>
         <p>software functionality. Proficient in software development and database management </p>
         <p>with a strong foundation in teamwork and problem-solving.</p>
-        <button>RESUME</button>
+        {borderMagicButton} 
         
       </div>
 
