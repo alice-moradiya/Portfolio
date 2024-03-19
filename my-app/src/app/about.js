b62817423132b2184b6e5e13e80854e76cec8d03
Alice <alice.al.moradiya@gmail.com>
2024-03-19 12:25:48 -0600

    configuring more about and home section content

--- my-app/src/app/about.js
+++ my-app/src/app/about.js
@@ -7,6 +7,7 @@ const About = () => {
         <h1>About Me</h1>
         <p>Hi, I am Alice.</p>
         <p>I am a student at the Concordia University of Edmonton, pursuing a major in Information Technology and a minor in Mathematics. </p>
+        <div className='skills-content'>
         <ul>
           <li>Languages: Python/ Java/ C++/ PHP </li>
           <li>Frameworks: Next.js/ Bootstrap/ React.js/ ASP.NET</li>
@@ -14,11 +15,13 @@ const About = () => {
           <li>WordPress/ Restful APIs/  Git/ GitHub</li>
    
         </ul>
+        </div>
         <p>Beyond my technical skills, I am a strong communicator and collaborator, eager to contribute to interdisciplinary teams and engage in meaningful projects.</p>
+        <p>Let's connect and create something impactful together.</p>
         
       </div>
-      <div className="about-image">
-        <img src={"/profile.png"} alt="[Alice photo]" />
+      <div className="about-image-container">
+        <img className="about-image" src={"/profile.png"} alt="[Alice photo]" />
       </div>
     </div>
   );
