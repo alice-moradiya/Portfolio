ee5e291fa1885637cf36bce1c6694877ab94a269
Alice <alice.al.moradiya@gmail.com>
2024-03-20 00:24:33 -0600

    Adding more content in education section

--- my-app/src/app/education.js
+++ my-app/src/app/education.js
@@ -1,18 +1,19 @@
 import React from "react";
+import { buttons } from '@/app/utils/components/ui/tailwindcss-buttons.tsx';
+
 const education = () => {
+    const Githubbutton = buttons.find(button => button.name === 'Shimmer').component;
     return(
     <div id="Education" className="education-section">
         <div className="university-logo-container">
             <img src="/Concordia.png" alt="Concordia University of Edmonton" className="university-logo" />
         </div>
         <div className="education-info">
-            <h2>Education</h2>
-            <h3>BSc. in Information Technology</h3>
-            <p>Concordia University of Edmonton</p>
-            <p>December 2020 – December 2024</p>
+            <h3>BSc. in Information Technology | 2020 – 2024</h3>
             <p>Major: Information Technology | Minor: Mathematics</p>
-            <p>Relevant Courses: Software Development, Web Development, Economics, Database Management, Networking, Business, Cybersecurity</p>
-            <p>Key Projects: Travel-agency-website (PHP), Secure-chat-app (Python), Course-registration-system (Python)</p>
+            <p>Relevant Courses: Software Development, Web Development, Database Management, Networking, Cybersecurity, Business, Economics</p>
+            <p>University Projects: Travel-agency-website (PHP), Secure-chat-app (Python), Course-registration-system (Python)</p>
+            {Githubbutton}
         </div>
     </div>
     )
