32e01c9e7e458f01b1b9eb637092727da51a302e
Alice <alice.al.moradiya@gmail.com>
2024-03-21 22:46:49 -0600

    Adding more content to skills section

--- my-app/src/app/skills.js
+++ my-app/src/app/skills.js
@@ -3,20 +3,33 @@ import React from 'react';
 const skills = {
 
   Skills: [
-    { name: 'ReactJS', icon: '/react.png' },
     { name: 'NextJS', icon: '/next.png' },
+    { name: 'ReactJS', icon: '/react.png' },
     { name: 'Bootstrap', icon: '/bootstrap.png' },
     { name: 'Android', icon: '/Android.png' },
+    { name: 'Tailwind CSS', icon: '/Tailwind.png' },
     { name: 'Java', icon: '/java.png' },
     { name: 'C++', icon: '/C++.png' },
     { name: 'Javascript', icon: '/js.png' },
-    
-    
-    { name: 'PHP', icon: '/icons/PHP.png' },
-    { name: 'PHP', icon: '/icons/PHP.png' },
-    { name: 'PHP', icon: '/icons/PHP.png' },
-    { name: 'PHP', icon: '/icons/PHP.png' },
+    { name: 'Python', icon: '/python.png' },
 
+
+    { name: 'ASP.Net', icon: '/Asp.png' },
+    { name: 'AWS', icon: '/AWS.png' },
+    { name: 'CSS', icon: '/CSS.png' },
+    { name: 'GitHub', icon: '/Github.png' },
+    { name: 'Git VCS', icon: '/Git.png' },
+    { name: 'HTML', icon: '/HTML.png' },
+    { name: 'Kali Linux', icon: '/Kalilinux.png' },
+    { name: 'MaterialUI', icon: '/Material.png' },
+    { name: 'Office Suite', icon: '/MS.png' },
+    { name: 'MySQL', icon: '/Mysql.png' },
+    { name: 'PHP', icon: '/PHP.png' },
+    { name: 'ReactBootstrap', icon: '/Reactboot.png' },
+    { name: 'Rest APIs', icon: '/Rest.png' },
+    { name: 'ShadecnUI', icon: '/Shadecn.png' },
+    { name: 'WordPress', icon: '/Wordpress.png' },
+    
     // ... other frameworks
   ],
 };
@@ -24,7 +37,7 @@ const skills = {
 const Skills = () => {
   return (
     <section id='skills'  className="bg-dark-section text-white py-8">
-      <h2 className="text-center text-3xl font-bold my-5">Skills</h2>
+      <h2 className="text-center text-3xl font-bold my-5">Skills & Abilities</h2>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 px-5">
         {Object.entries(skills).flatMap(([category, skillList]) =>
           skillList.map(skill => (
