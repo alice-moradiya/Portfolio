87ed37470263297153b266deb11ddcb920a21fab
Alice <alice.al.moradiya@gmail.com>
2025-04-10 02:09:59 -0600

    updates

--- my-app/src/app/home.js
+++ my-app/src/app/home.js
@@ -43,12 +43,12 @@ const HomePageContent = () => {
 
       <div id="infobuttons"><LinkedIn />
         {/* Resume button */}
-        <a href='/Resume.pdf' id="bordermagic" className="no-underline relative inline-flex h-12 overflow-hidden rounded-full p-0.5 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+       {/* <a href='/Resume.pdf' id="bordermagic" className="no-underline relative inline-flex h-12 overflow-hidden rounded-full p-0.5 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
           <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+           <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
             Resume
-          </span>
-        </a>
+          </span> 
+        </a>*/}
         <Github />
       </div>
     </div>
