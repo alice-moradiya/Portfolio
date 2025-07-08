ba4a378146096a65afa5b81478272fa0acc4b233
Alice <alice.al.moradiya@gmail.com>
2025-07-07 21:52:36 -0600

    updates

--- my-app/src/app/home.js
+++ my-app/src/app/home.js
@@ -34,15 +34,14 @@ const HomePageContent = () => {
   return (
     <div id="home" className="relative z-5 text-center text-white pt-[185px] pb-[163px]">
       <h1 className="text-[72px] text-shadow style={{ textShadow: '0 0 6px rgba(255, 255, 255, 0.8)' }}">Alice Moradiya</h1>
-      <h3 className="text-[28px]"> I am into {jobTitle}|</h3>
       <br/>
+      <h3 className="text-[28px]"> I am into {jobTitle}|</h3>
       <h2 className="text-[32px]"> <img src="/Web-img.png" width="35" height="35" className="d-inline-block align-top" alt="Logo" /> Web Developer |
         <img src="/Soft-img.png" width="35" height="35" className="d-inline-block align-top" alt="Logo" /> Software Developer</h2>
       <br />
       <p>⚡ Dynamic Web Developer with a track record of driving user adoption and enhancing software functionality.</p>
       <p>Proficient in Web development 🖥️ and database management 📊 with a strong foundation in teamwork.</p>
       <p>🔍 Skilled in problem-solving to deliver effective solutions.</p>
-      <br />
       <div id="infobuttons"><LinkedIn />
         {/* Resume button */}
         {/* <a href='/Resume.pdf' id="bordermagic" className="no-underline relative inline-flex h-12 overflow-hidden rounded-full p-0.5 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
